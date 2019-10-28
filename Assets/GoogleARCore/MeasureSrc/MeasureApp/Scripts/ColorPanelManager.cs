61067b26020d79442d48865ff618df5ca03c856d
Yonatal123 <ytarlovs@gmail.com>
2019-10-28 22:26:16 +0200

    Added color picker

---
+++ Assets/GoogleARCore/MeasureSrc/MeasureApp/Scripts/ColorPanelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPanelManager : MonoBehaviour
+{
+    public GameObject ColorPanel;
+    // Start is called before the first frame update
+
+    public void OpenPanel()
+    {
+        ColorPanel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        closePanel();
+    }
+    void Start()
+    {
+        closePanel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void closePanel()
+    {
+        ColorPanel.SetActive(false);
+    }
+}
